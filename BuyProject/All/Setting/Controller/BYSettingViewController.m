4f3c080e3a25f579700cda163d96a9b1fa440657
LiluluSelf <13641138156@163.com>
2016-07-07 14:56:47 +0800

    设置界面

--- BuyProject/All/Setting/Controller/BYSettingViewController.m
+++ BuyProject/All/Setting/Controller/BYSettingViewController.m
@@ -7,7 +7,79 @@
 //
 
 #import "BYSettingViewController.h"
+#import "MacroDefine.h"
+#import "UIScrollView+TwitterCover.h"
+@interface BYSettingViewController ()<UITableViewDataSource,UITableViewDelegate,UINavigationControllerDelegate>
+{
+    UIView *topView;
+}
+@property (nonatomic, strong) UITableView *tableViewSetting;
+@property (nonatomic, strong) NSMutableArray *mutableArraySettings;
 
+@end
 @implementation BYSettingViewController
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.title=@"我";
+    }
+    return self;
+}
+- (void)share
+{
+    
+}
+
+- (void)viewDidLoad
+{
+    self.navigationItem.leftBarButtonItem=[[UIBarButtonItem alloc]initWithTitle:@"分享" style:UIBarButtonItemStylePlain target:self action:@selector(share)];
+    self.mutableArraySettings = [NSMutableArray arrayWithObjects:[NSArray arrayWithObjects:@"我的夺宝记录",@"我的夺宝码",@"获得的商品",@"收货地址 ", nil],[NSArray arrayWithObjects:@"设置",nil], nil];
+    
+    UITableView *tableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREEN.width, SCREEN.height-self.tabBarController.tabBar.frame.size.height) style:UITableViewStyleGrouped];
+    tableView.dataSource=self;
+    tableView.delegate=self;
+    [self.view addSubview:tableView];
+    self.tableViewSetting = tableView;
+    
+    [self.tableViewSetting addTwitterCoverWithImage:[UIImage imageNamed:@"usr_bg"] withTopView:topView];
+    
+    self.tableViewSetting.tableHeaderView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN.width, CHTwitterCoverViewHeight+topView.bounds.size.height)];
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+
+
+#pragma mark - tableview
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.mutableArraySettings.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSArray *array = self.mutableArraySettings[section];
+    return array.count;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CellSettings";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+    }
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    NSArray *array = self.mutableArraySettings[indexPath.section];
+    cell.textLabel.text = array[indexPath.row];
+    
+    return cell;
+}
 
 @end
--- BuyProject/internet/MacroDefine.h
+++ BuyProject/internet/MacroDefine.h
@@ -11,5 +11,7 @@
 
 
 #endif /* MacroDefine_h */
+#define SCREEN [UIScreen mainScreen].bounds.size
+
 #define APP_COLOR [UIColor colorWithRed:250.0/255.0 green:102.0/255.0 blue:34.0/255.0 alpha:1.0] //COLOR(56, 63, 72)
 #define COLOR(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
